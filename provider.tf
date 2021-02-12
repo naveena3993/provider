52b3584258bd3298e0ac47703cd846474450e50a
naveena3993 <54440527+naveena3993@users.noreply.github.com>
2021-02-12 17:46:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,8 @@
 provider "aws" {
-  access_key = "AKIAIWDND3P5GXUWAMRQ"
-  secret_key = "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"
+  access_key = "AKIAUIHVAPP3UMZO6INZ"
+  secret_key = "jeRhlR4bgpWbSA0w61d0a8nkGhm+Gj8KgZmG3jqz"
   region     = "us-east-1"
 }
-
 resource "aws_instance" "example" {
   ami           = "ami-00ddb0e5626798373"
   instance_type = "t2.micro"
